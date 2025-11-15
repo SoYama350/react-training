a04cf025863164fa31222cf072fa366acbf8a3f9
SoYama350 <xcanem@gmail.com>
2025-11-15 21:55:10 +0200

    Auto update

--- 06-goal-tracker/src/App.js
+++ 06-goal-tracker/src/App.js
@@ -1,19 +1,59 @@
-import './App.css';
+import { useState } from 'react';
+const initialGoals = [{
+  id: 15515131,
+  name: "catch",
+  check: false,
+},
+{
+  id: 4464,
+  name: "dasda",
+  check: false,
+},
+{
+  id: 565555,
+  name: "sdasdas",
+  check: true,
+},
+{
+  id: 59952,
+  name: "caasdasdtch",
+  check: true,
+},
+{
+  id: 6511,
+  name: "asdasdac",
+  check: false,
+}];
+export default function App() {
+  const [inputGoal, setInputGoal] = useState(initialGoals); // lefting state from GoalForm
 
-function App() {
+
+  function handleAddGoal(goal) {
+    setInputGoal((goals) => [...goals, goal]);
+  }
+
+
+
   return (
     <div className="App">
-      <MainBody />
+      <MainBody inputGoal={inputGoal} />
+      <FormAddGoal onAddGoal={handleAddGoal} />
     </div>
   );
 }
 
-export default App;
 
-
-
+    function GoalList({goals}) {
+    return (
+      <ul className='goals'>
+        {goals.map((goal) =>
+          <Task key={goal.id} goal={goal.name} />)}
+      </ul>
+    )
+  }
 // this func is the main body for the tracking app
-function MainBody() {
+function MainBody({inputGoal}) {
+
   return (
     <div>
       <ul className='days'>
@@ -25,30 +65,53 @@ function MainBody() {
         <li>thu</li>
         <li>fri</li>
       </ul>
-      <ul className='goals'>
-        <AddingGoals />
-      </ul>
+      <GoalList goals={inputGoal} />
+      
     </div>
   )
 }
+  
+  
+function Task({ goal }) {
+  const [check, setCheck] = useState(false);
 
-// this func is to convert the inputed goal to a jsx
-function AddingGoals() {
   return (
-    <li>{inputGoals}</li>
+    <li>
+    <div>
+      <h2>{goal}</h2><input type='checkbox' value={check} onChange={(e) => setCheck(e.target.value)}/>
+      <p>have you done this today ? </p>
+      <button onClick={() => setCheck(true)}>close</button>
+
+  
+    </div>
+    </li>
   )
 }
 
-// this func is to get the goal from the user to add it to the tracker
-function GoalForm() {
-  return (
+function FormAddGoal({ onAddGoal }) {
+  const [name, setName] = useState("");
+
+  function handleSubmit(e) {
     
+    e.preventDefault();
+
+    let id = crypto.randomUUID();
+    const newGoal = {
+      id,
+      name,
+    };
+
+    onAddGoal(newGoal);
+
+    setName("");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Enter Your Goal : </label>
+      <input type='text' placeholder='hit the gym' value={name} onChange={(e) => setName(e.target.value)} />
+      <button>Add</button>
+    </form>
   )
 }
 
-// this func is to toggle the goal (done or not)
-function GoalToggle() {
-  return (
-    
-  )
-}
+
